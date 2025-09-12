7a5ee61096c6aa35c23916f2e5ef230efd233bf1
Angus Dobson <angus.dobson@azenta.com>
2025-09-12 03:45:11 -0400

    Remove local development files from repository

--- sync-to-windows.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Sync VibeCat project to Windows C: drive
-
-echo "Syncing VibeCat to C:\\vibecat..."
-rsync -av --delete /home/adobson/source/repos/vibecat/VibeCat/ /mnt/c/vibecat/
-echo "Sync complete! Project available at C:\\vibecat"
